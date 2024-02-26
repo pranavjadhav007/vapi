4db3f17feec900b455a00e6badbe0d27f812ca20
Pranav Jadhav <88050656+pranavjadhav007@users.noreply.github.com>
2024-02-26 09:59:40 +0530

    test

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use("/", (req, res) => {
 });
 
 app.use("/api/v1/reservation", reservationRouter);
-app.get("/", (req, res, next)=>{return res.status(200).json({
+app.get("/api/reservation", (req, res, next)=>{return res.status(200).json({
   success: true,
   message: "Route Connected"
 })})
