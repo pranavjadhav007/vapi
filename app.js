d1bfe2adb94c652de57f8cfe4a7a27667a071bf3
Pranav Jadhav <88050656+pranavjadhav007@users.noreply.github.com>
2024-02-26 09:45:32 +0530

    test

--- app.js
+++ app.js
@@ -1,40 +1,62 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import { errorMiddleware } from "./middlewares/error.js";
+// import reservationRouter from "./routes/reservationRoute.js";
+// import { dbConnection } from "./database/dbConnection.js";
+
+// const app = express();
+// dotenv.config({ path: "./config.env" });
+
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ["POST"],
+//     credentials: true,
+//   })
+// );
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use("/api/v1/reservation", reservationRouter);
+// app.get("/", (req, res, next)=>{return res.status(200).json({
+//   success: true,
+//   message: "Route Connected"
+// })})
+// app.use("*", (req, res) => {
+//   res.status(404).json({
+//     success: false,
+//     message: "Route not found",
+//   });
+// });
+// dbConnection();
+
+// app.use(errorMiddleware);
+
+// app.listen(process.env.PORT, ()=>{
+//   console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
+// })
+
+// export default app;
+
 import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { errorMiddleware } from "./middlewares/error.js";
 import reservationRouter from "./routes/reservationRoute.js";
-import { dbConnection } from "./database/dbConnection.js";
 
 const app = express();
-dotenv.config({ path: "./config.env" });
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST"],
-    credentials: true,
-  })
-);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/reservation", reservationRouter);
-app.get("/", (req, res, next)=>{return res.status(200).json({
-  success: true,
-  message: "Route Connected"
-})})
+
 app.use("*", (req, res) => {
   res.status(404).json({
     success: false,
     message: "Route not found",
   });
 });
-dbConnection();
 
-app.use(errorMiddleware);
-
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT, () => {
   console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
-})
+});
 
 export default app;
--- routes/reservationRoute.js
+++ routes/reservationRoute.js
@@ -1,8 +1,20 @@
+// import express from "express";
+// import send_reservation from "../controller/reservation.js";
+
+// const router = express.Router();
+
+// router.post("/send", send_reservation);
+
+// export default router;
 import express from "express";
-import send_reservation from "../controller/reservation.js";
 
 const router = express.Router();
 
-router.post("/send", send_reservation);
+router.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Test route",
+  });
+});
 
 export default router;
