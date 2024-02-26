2d72cc20d0f600056f74e27a225bc2115a800615
Pranav Jadhav <88050656+pranavjadhav007@users.noreply.github.com>
2024-02-26 10:11:58 +0530

    test

--- app.js
+++ app.js
@@ -1,47 +1,68 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import { errorMiddleware } from "./middlewares/error.js";
+// import reservationRouter from "./routes/reservationRoute.js";
+// import { dbConnection } from "./database/dbConnection.js";
+
+// const app = express();
+// dotenv.config({ path: "./config.env" });
+
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ["POST"],
+//     credentials: true,
+//   })
+// );
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use("/", (req, res) => {
+//   res.status(200).json({
+//     success: true,
+//     message: "true",
+//   });
+// });
+
+// app.use("/api/v1/reservation", reservationRouter);
+
+// app.use("*", (req, res) => {
+//   res.status(404).json({
+//     success: false,
+//     message: "Route not found",
+//   });
+// });
+// dbConnection();
+
+// app.use(errorMiddleware);
+
+// app.listen(process.env.PORT, ()=>{
+//   console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
+// })
+
+// export default app;.
+
 import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { errorMiddleware } from "./middlewares/error.js";
-import reservationRouter from "./routes/reservationRoute.js";
-import { dbConnection } from "./database/dbConnection.js";
 
 const app = express();
-dotenv.config({ path: "./config.env" });
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST"],
-    credentials: true,
-  })
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.status(200).json({
     success: true,
-    message: "true",
+    message: "Test route",
   });
 });
 
-app.use("/api/v1/reservation", reservationRouter);
-app.get("/api/reservation", (req, res, next)=>{return res.status(200).json({
-  success: true,
-  message: "Route Connected"
-})})
 app.use("*", (req, res) => {
   res.status(404).json({
     success: false,
     message: "Route not found",
   });
 });
-dbConnection();
 
-app.use(errorMiddleware);
-
-app.listen(process.env.PORT, ()=>{
-  console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT || 3000}`);
+});
 
 export default app;
